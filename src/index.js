d5270211d0c3deb2f9edbaadb8d84a7f20c3c629
maksagit <maksibon@gmail.com>
2018-03-05 18:08:04 +0300

    ZerosCount test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+  var zeroCount = number;
+
+  for (var i = 2; i <= base; i++) {
+    if (base % i == 0) {
+      var pow = 0;
+
+      while (base % i == 0) {
+        pow++;
+        base = Math.floor(base/i)
+      }
+
+      var c = 0;
+      var x = number;
+
+      while (x/i > 0) {
+        c += Math.floor(x/i);
+        x = Math.floor(x/i);
+      }
+
+      zeroCount = Math.min(zeroCount, Math.floor(c/pow));
+    }
+  }
+
+  return zeroCount;
 }
